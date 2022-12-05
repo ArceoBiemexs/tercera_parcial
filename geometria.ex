dedf98d65e9e8a2202f18233ce90392dded36e5f
Arceo Jacobo Oswaldo <111711802+ArceoBiemexs@users.noreply.github.com>
2022-12-04 22:00:26 -0600

    Create geometria.ex

---
+++ geometria.ex
@@ -0,0 +1,15 @@
+#   UNIVERSIDAD DE COLIMA
+# OSWALDO DANIEL ARCEO JACOBO 3 "D" ICI
+
+
+defmodule Geometria.Cuadrado do
+  def perimetro(l) do
+    4*l
+  end
+end
+
+defmodule Geometria.Rectangulo do
+  def perimetro(l1,l2) do
+    2*l1 + 2*l2
+  end
+end
