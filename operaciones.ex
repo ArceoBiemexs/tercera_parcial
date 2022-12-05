afc1b8cc77351687446aaaa02efdd2162582bfc5
Arceo Jacobo Oswaldo <111711802+ArceoBiemexs@users.noreply.github.com>
2022-12-04 22:19:34 -0600

    Create operaciones.ex

---
+++ operaciones.ex
@@ -0,0 +1,8 @@
+#   UNIVERSIDAD DE COLIMA
+# OSWALDO DANIEL ARCEO JACOBO 3 "D" ICI
+# Obtener el cuadrado de la suma de 2 números (8 y 9)
+
+defmodule Operaciones do
+  def suma(n1, n2), do: n1 + n2
+  def cuadrado(n), do: n * n
+end
