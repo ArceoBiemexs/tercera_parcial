81e7d86208acd8f1fd0e2160fb14fad0b236120e
Arceo Jacobo Oswaldo <111711802+ArceoBiemexs@users.noreply.github.com>
2022-12-04 21:53:11 -0600

    Update area_de.ex

--- area_de.ex
+++ area_de.ex
@@ -2,6 +2,7 @@
 # OSWALDO DANIEL ARCEO JACOBO 3 "D" ICI
 
 # defmodule inicia con mayus
+# trabajmos con modulos, el archivo puede contener varios modulos
 
 defmodule Areas do
   def area_cuadrado(1) do
