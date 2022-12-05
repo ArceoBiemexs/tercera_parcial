f51c12eff6e6b1b77a9985842427683db12b3f91
Arceo Jacobo Oswaldo <111711802+ArceoBiemexs@users.noreply.github.com>
2022-12-04 22:01:22 -0600

    Update geometria.ex

--- geometria.ex
+++ geometria.ex
@@ -13,3 +13,17 @@ defmodule Geometria.Rectangulo do
     2*l1 + 2*l2
   end
 end
+### OTRA MANERA DE TRABAJAR CON MODULOS
+defmodule Geometria do
+  defmodule Cuadrado do
+    def perimetro(l) do
+      4*l
+    end
+  end
+
+  defmodule Rectangulo do
+    def perimetro(l1,l2) do
+      2*l1 + 2*l2
+    end
+  end
+end
