33ad46b4bfd0e25f00742ca3a09489d2fc4c6df3
Arceo Jacobo Oswaldo <111711802+ArceoBiemexs@users.noreply.github.com>
2022-12-04 21:41:14 -0600

    Create hello_world.ex

---
+++ hello_world.ex
@@ -0,0 +1,13 @@
+# UNIVERSIDAD DE COLIMA 3 "D" ICI
+# ARCEO JACOBO OSWALDO DANIEL
+
+# • Función sin argumentos (procedimiento)
+
+defmodule HolaMundo do
+  
+    def mensaje do
+  
+      IO.puts("Hola Mundo")
+  
+  end
+end
