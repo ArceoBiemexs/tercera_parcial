274922baefd158d3f2fdfecf5204af10ccccf346
Arceo Jacobo Oswaldo <111711802+ArceoBiemexs@users.noreply.github.com>
2022-12-04 22:37:57 -0600

    Create arity.ex

---
+++ arity.ex
@@ -0,0 +1,14 @@
+#   UNIVERSIDAD DE COLIMA
+# OSWALDO DANIEL ARCEO JACOBO 3 "D" ICI
+
+# Aridad de funciones Es el nombre para el número de argumentos que una función recibe una función se identifica por:
+# 1. el módulo donde se encuentra, 2. su nombre y 3. su aridad
+
+defmodule Rectangulo do
+  def area(1) do
+    1 *1
+  end
+  def area(l1,l2) do
+    l1 * l2
+  end
+end
